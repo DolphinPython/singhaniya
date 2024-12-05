2bcacac13516c41c8c24a2f04c976a165f5f3e0a
Dolphin <pythondolphin@gmail.com>
2024-12-05 18:13:08 +0530

    first commit

---
+++ websites.php
@@ -0,0 +1,30 @@
+Laravel
+https://kvizzing.com/
+https://www.asiandispatch.net/
+
+Wordpress
+https://www.phdcci.in/
+http://phdcci-centreforsustainability.com/
+https://heroindianopen.com/
+https://logisticshakti.com
+https://ihwcouncil.org/
+https://qualitybharat.qcin.org/
+https://seniorsinseva.org/
+https://smefutures.com/
+
+Core PHP
+https://rappidx.com/
+https://www.ihwcouncil.org/digitalhealthsummit/index.php
+https://vitiligofoundationindia.org/
+https://rngfoundation.com/awards/
+https://travelswithhappiness.com/
+
+Shopify
+https://explorehonor.com/
+https://parfumfrance.com/
+https://vazestore.fr/
+https://mayufrance.fr/
+
+Frontend & Wix
+https://magicnation.in/             -   Frontend(phpbb platform)
+https://www.kwalitysshiraz.com/     -   WIX
